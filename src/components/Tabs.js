7b3341f3d89942d8a8547a5c81e9e54fd4357002
MohamedArif20091999 <mohamedarifcbe@gmail.com>
2021-06-22 16:28:07 +0530

    feat 1. Seperated header and query params feature

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -20,38 +20,7 @@ const Tabs = ({
     console.log(queryParam);
     return (
       <div key={i}>
-        <div id="test1" className="col s12">
-          <div class="input-field col s5">
-            <input
-              placeholder="key"
-              id="first_name"
-              type="text"
-              class="validate"
-              onChange={(e) => handleQueryParams(e.target.value, i, "key")}
-            />
-          </div>
-          <div class="input-field col s5">
-            <input
-              placeholder="value"
-              id="last_name"
-              type="text"
-              class="validate"
-              onChange={(e) => handleQueryParams(e.target.value, i, "value")}
-            />
-          </div>
-          <div className="delete-btn col s1">
-            <a
-              onClick={(e) => removeQueryParam(i)}
-              class="btn-floating btn-small  waves-effect waves-light grey"
-            >
-              <i class="material-icons">delete</i>
-            </a>
-          </div>
-        </div>
-
-        {/* HEADERS */}
-
-        <div id="test1" className="col s12">
+        <div className="col s12">
           <div class="input-field col s5">
             <input
               placeholder="key"
@@ -83,6 +52,42 @@ const Tabs = ({
     );
   });
 
+  const renderHeaderItems = headerParams.map((headerParam, i) => {
+    return (
+      //  HEADERS
+      <div key={i}>
+        <div className="col s12">
+          <div class="input-field col s5">
+            <input
+              placeholder="key"
+              id="first_name"
+              type="text"
+              class="validate"
+              onChange={(e) => handleHeaderParams(e.target.value, i, "key")}
+            />
+          </div>
+          <div class="input-field col s5">
+            <input
+              placeholder="value"
+              id="last_name"
+              type="text"
+              class="validate"
+              onChange={(e) => handleHeaderParams(e.target.value, i, "value")}
+            />
+          </div>
+          <div className="delete-btn col s1">
+            <a
+              onClick={(e) => removeHeaderParam(i)}
+              class="btn-floating btn-small  waves-effect waves-light grey"
+            >
+              <i class="material-icons">delete</i>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="col s12">
@@ -97,15 +102,29 @@ const Tabs = ({
           </li>
         </ul>
       </div>
-      {renderItems}
-      <div>
-        <a
-          onClick={(e) => addQueryParamField()}
-          class="btn-floating btn-small  waves-effect waves-light  right-align"
-        >
-          <i class="material-icons">add</i>
-        </a>
+      <div id="test1">
+        {renderItems}
+        <div>
+          <a
+            onClick={(e) => addQueryParamField()}
+            class="btn-floating btn-small  waves-effect waves-light  right-align"
+          >
+            <i class="material-icons">add</i>
+          </a>
+        </div>
       </div>
+      <div id="test2">
+        {renderHeaderItems}
+        <div>
+          <a
+            onClick={(e) => addHeaderParamField()}
+            class="btn-floating btn-small  waves-effect waves-light  right-align"
+          >
+            <i class="material-icons">add</i>
+          </a>
+        </div>
+      </div>
+
       {/* <div className="col s12">
         <ul className="tabs">
           <li className="tab col s6">
